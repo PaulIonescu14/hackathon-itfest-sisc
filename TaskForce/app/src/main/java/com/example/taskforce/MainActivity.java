ee36554bfa9ee76629f7a656dabe11b968f9a934
Paul Ionescu <paulionescu2005@yahoo.ro>
2025-11-08 09:13:17 +0200

    Added backend for login

--- TaskForce/app/src/main/java/com/example/taskforce/MainActivity.java
+++ TaskForce/app/src/main/java/com/example/taskforce/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.taskforce;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +25,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        EditText password = (EditText) findViewById(R.id.editTextTextPassword);
+        Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        Switch togglePass = (Switch) findViewById(R.id.switchPassView);
+        TextView forgotPass = (TextView) findViewById(R.id.textViewForgotPass);
+        TextView goToRegister = (TextView) findViewById(R.id.textViewGoToRegister);
+
+        buttonLogin.setOnClickListener(v -> {
+           String emailText = email.getText().toString();
+           String passText = password.getText().toString();
+
+            Log.d("MainActivity", "email: " + emailText);
+            Log.d("MainActivity", "password: " + passText);
+
+        });
+
+        togglePass.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked) {
+                password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            } else {
+                password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            }
         });
     }
+
+
+
+
 }
